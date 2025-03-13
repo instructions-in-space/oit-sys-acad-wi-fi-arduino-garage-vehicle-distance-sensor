153289a95a90814cf01abf59f069482a8366a4f0
Mark Mumford <mem27@byu.edu>
2025-03-13 16:53:38 -0600

    Can talk to each other; still can't detect string

--- distance-sensor/distance-sensor.ino
+++ distance-sensor/distance-sensor.ino
@@ -1,9 +1,88 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266HTTPClient.h>
+#include "wi-fi_config.h"
 
+// Create an instance of the server (on port 80)
+ESP8266WebServer server(80);
+
+String lastHostIP = "";  // Variable to store the IP of the other host
+
+void setup() {
+  // Start serial communication for debugging
+  Serial.begin(115200);
+
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  Serial.println("");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(".");
+  }
+  Serial.println("Connected to Wi-Fi");
+
+  // Print the local IP address
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+
+  // Define the GET request handler
+  server.on("/", HTTP_GET, handleRequest);
+
+  // Start the server
+  server.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  // Handle incoming HTTP requests
+  server.handleClient();
 }
+
+void handleRequest() {
+  // Get the request URL
+  String request = server.uri();
+  
+  // Get the IP address of the requesting host
+  lastHostIP = server.client().remoteIP().toString();
+  Serial.print("Request received from IP: ");
+  Serial.println(lastHostIP);
+
+  // Check if the request contains "redlight"
+  if (request.indexOf("redlight") != -1) {
+    // If so, send a GET request to the other host with "greenlight"
+    String greenlightURL = "http://" + lastHostIP + "/?greenlight";
+    sendGreenLightRequest(greenlightURL);
+
+    // Respond back with a confirmation message
+    server.send(200, "text/plain", "Received redlight, sent greenlight request to " + lastHostIP);
+    Serial.print("Received 'redlight' message; responded with 'greenlight'.");
+    Serial.print(lastHostIP);
+  } else {
+    // If the request doesn't contain "redlight", respond with a different message
+    server.send(200, "text/plain", "Request received but doesn't contain 'redlight'");
+    server.send(200, "text/plain", request);
+    Serial.print(request);
+  }
+}
+
+void sendGreenLightRequest(String url) {
+  // Use HTTPClient to send a GET request to the other host
+  WiFiClient client;
+  HTTPClient http;
+
+  // Send the GET request
+  http.begin(client, url);  // URL to send the GET request to
+  int httpCode = http.GET(); // Send the GET request
+
+  // Print the response code
+  if (httpCode > 0) {
+    Serial.print("HTTP GET Response code: ");
+    Serial.println(httpCode);
+  } else {
+    Serial.print("HTTP GET request failed, error: ");
+    Serial.println(http.errorToString(httpCode).c_str());
+  }
+
+  // Close the connection
+  http.end();
+}
+
--- stoplight/stoplight.ino
+++ stoplight/stoplight.ino
@@ -5,183 +5,63 @@
   #include <ESP8266HTTPClient.h>
   #include "wi-fi_config.h"
 
-  // Define pins for LEDs
-	const int redLED = 16;
-	const int yellowLED = 14;
-	const int greenLED = 12;
+// Variable to store the device's IP address that responds with "greenlight"
+String targetIP = "";
 
-  // Create a server to listen on port 80
-  WiFiServer server(80);
-
-  // Starting IP address (you can tweak these values based on your network's configuration)
-  String baseIP = "192.168.204.";  // The subnet of your network
-  int startRange = 2;   // Starting IP address range (Usually 192.168.1.2 onwards)
-  int endRange = 255;   // Ending IP address range (You can limit this for faster scanning)
-
-	void setup() {
-     // Setup serial port
-    Serial.begin(115200);
-    Serial.println();
-
-    // Begin Wi-Fi 
-    WiFi.begin(WIFI_SSID,WIFI_PASS);
-    // I may not want this section after all, but I'll keep it here for now.
-    while (WiFi.status() != WL_CONNECTED) {
-      delay(1000);
-      Serial.println("Connecting to WiFi...");
-    }
-    Serial.println("Connected to WiFi");
-    Serial.print("IP Address: ");
-    Serial.println(WiFi.localIP());  // Print the IP address assigned by DHCP
-
-    // Start scanning local network
-    scanNetwork();
-
-    // Start the server
-    server.begin();
-
-	  // Set each LED pin as an OUTPUT
-	  pinMode(redLED, OUTPUT);
-	  pinMode(yellowLED, OUTPUT);
-	  pinMode(greenLED, OUTPUT);
-	}
-
-  void scanNetwork() {
-    // Try GET request to each IP in the local subnet
-    for (int i = startRange; i <= endRange; i++) {
-      String ip = baseIP + String(i);
-      sendGETRequest(ip);
-      delay(500);  // Add delay to avoid flooding the network
-    }
+// Function to connect to Wi-Fi
+void connectToWiFi() {
+  Serial.println("Connecting to Wi-Fi...");
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(".");
   }
-
-  void sendGETRequest(String ip) {
-    String url = "http://" + ip + "/request";  // Customize this URL based on your device's expected endpoint
-  
-  // Set up HTTP Client
-  HTTPClient http;
-  begin(WiFiClient, url);
-  int httpCode = http.GET();
-  
-  // Check if the HTTP request was successful
-  if (httpCode > 0) {
-    Serial.print("HTTP GET request to ");
-    Serial.print(ip);
-    Serial.print(" successful. Response code: ");
-    Serial.println(httpCode);
-
-    String payload = http.getString();
-    Serial.print("Response: ");
-    Serial.println(payload);
-    
-    // You can add custom logic to identify the correct device based on the response payload
-    if (payload.indexOf("YourUniqueDeviceIdentifier") >= 0) {
-      Serial.println("Target device found!");
-    }
-  } else {
-    Serial.print("HTTP GET request failed for ");
-    Serial.print(ip);
-    Serial.print(". Error code: ");
-    Serial.println(httpCode);
-  }
-
-  http.end();
+  Serial.println("Connected to Wi-Fi");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
-
-
-
-
-	void loop() {
-    WiFiClient client = server.available();  // Listen for incoming clients
-
-    if (client) {
-      // Serial.println("New Client");
-      // Wait until the client sends some data
-      while (client.connected()) {
-        if (client.available()) {
-          String raw_command = client.readStringUntil('\n');
-          client.flush();
-          const char* web_command = raw_command.c_str();  // Converts true string into char array (C string)
-          //Serial.print(web_command);
-          //Serial.println();
-
-          // -----------------------------------------------------------------------------------
-          // Automatic mode off (all lights turn off)
-          if(strstr(web_command, "automaticoff")) {
-            //Serial.println();
-	          // All lights OFF
-	          digitalWrite(redLED, LOW);
-	          digitalWrite(yellowLED, LOW);
-	          digitalWrite(greenLED, LOW);
-            Serial.print("All lights off");
-            Serial.println();
-          }
-
-          // -----------------------------------------------------------------------------------
-          // *** Manual Modes ***
-
-          // Manual mode, red light on
-          if(strstr(web_command, "redon")) {
-            //Serial.println();
-	          // Red light ON
-	          digitalWrite(redLED, HIGH);
-            Serial.print("Red light on");
-            Serial.println();
-          }
-
-          // Manual mode, red light off
-          if(strstr(web_command, "redoff")) {
-            //Serial.println();
-	          // Red light OFF
-	          digitalWrite(redLED, LOW);
-            Serial.print("Red light off");
-            Serial.println();
-          }
-
-          // Manual mode, yellow light on
-          if(strstr(web_command, "yellowon")) {
-            //Serial.println();
-	          // Yellow light ON
-	          digitalWrite(yellowLED, HIGH);
-            Serial.print("Yellow light on");
-            Serial.println();
-          }
-
-          // Manual mode, yellow light off
-          if(strstr(web_command, "yellowoff")) {
-            //Serial.println();
-	          // Yellow light OFF
-	          digitalWrite(yellowLED, LOW);
-            Serial.print("Yellow light off");
-            Serial.println();
-          }
-
-          // Manual mode, green light on
-          if(strstr(web_command, "greenon")) {
-            //Serial.println();
-	          // Green light ON
-	          digitalWrite(greenLED, HIGH);
-            Serial.print("Green light on");
-            Serial.println();
-          }
-
-          // Manual mode, green light off
-          if(strstr(web_command, "greenoff")) {
-            //Serial.println();
-	          // Green light OFF
-	          digitalWrite(greenLED, LOW);
-            Serial.print("Green light off");
-            Serial.println();
-          }
-          // -----------------------------------------------------------------------------------
-          // Send a basic response back to the client
-          client.print("HTTP/1.1 200 OK\r\n");
-          //client.print("Content-Type: text/html\r\n\r\n");
-          //client.print("<html><body><h1>Hello, Wemos D1 Mini!</h1></body></html>");
-          break;
-        }
-      }
+void setup() {
+  Serial.begin(115200);
+  connectToWiFi();
+  
+  // Try IP addresses in the range 192.168.204.x (1 to 254)
+  Serial.print("Scanning for Arduino controlling the distance sensor.");
+  for (int i = 1; i <= 254; i++) {
+    String ip = "192.168.204." + String(i); // Generate the IP address
+    sendHttpRequest(ip);  // Send the HTTP request
+    Serial.println();
+    Serial.print("Scanning IP:");
+    Serial.print(ip);
+    if (targetIP != "") {
+      break;  // Exit if the target device is found
     }
-	}
+  }
+}
 
+void sendHttpRequest(String ip) {
+  WiFiClient client;
+  String url = "http://" + ip + "/?redlight"; // Send GET request to the device
+  if (client.connect(ip.c_str(), 80)) { // Connect to the device's IP address on port 80
+    client.print("GET /?redlight HTTP/1.1\r\n");
+    client.print("Host: " + ip + "\r\n");
+    client.print("Connection: close\r\n\r\n");
+    delay(500); // Wait for response
+    String response = client.readString();
+    Serial.println();
+    Serial.print("This is the response:  ");
+    Serial.print(response);
+    if (response.indexOf("greenlight") != -1) { // Check if the response is "greenlight"
+      targetIP = ip; // Store the IP address of the device
+      Serial.print("Found target device at IP: ");
+      Serial.println(targetIP);
+      client.stop();
+      return;
+    }
+    client.stop();
+  }
+}
+
+void loop() {
+  // Add code here if you need continuous functionality after the device is found
+}
