0ccb91db1d87d2d74ddc15e4f8898f19ec0c63b3
Mark Mumford <mem27@byu.edu>
2025-03-19 14:24:56 -0600

    Working with LEDs, but slow response

--- distance-sensor/distance-sensor.ino
+++ distance-sensor/distance-sensor.ino
@@ -2,26 +2,73 @@
 #include <ESP8266WebServer.h>
 #include "wi-fi_config.h"
 
+// - - - - - - - - - - - - - - - - (Begin)
+// Distance Sensor Code
+#define ECHO D2                 // attach pin D2 Arduino to pin Echo of HC-SR04
+#define TRIG D3                 // attach pin D3 Arduino to pin Trig of HC-SR04
+
+long duration;                  // variable for the duration of sound wave travel
+// - - - - - - - - - - - - - - - - (End)
+
+// - - - - - - - - - - - - - - - - (Begin)
+// Distance Sensor Code
+// - - - - - - - - - - - - - - - - (End)
+
+
 ESP8266WebServer server(80);
 
 void setup() {
+    // - - - - - - - - - - - - - - - - (Begin)
+    // Distance Sensor Code
+    pinMode(TRIG, OUTPUT);        // Sets the TRIG as an OUTPUT
+    pinMode(ECHO, INPUT);         // Sets the ECHO as an INPUT
+    //Serial.begin(9600);           // Serial Communication is starting with 9600 of baudrate speed
+    Serial.println("HC-SR04 Distance Sensor"); // print some text in Serial Monitor
+    // - - - - - - - - - - - - - - - - (End)
     Serial.begin(115200);
     WiFi.begin(WIFI_SSID, WIFI_PASS);
-
     Serial.print("Connecting to Wi-Fi");
     while (WiFi.status() != WL_CONNECTED) {
         delay(500);
         Serial.print(".");
     }
-
     Serial.println("\nConnected! IP Address: " + WiFi.localIP().toString());
-    
     server.on("/message", handleMessage);
     server.begin();
 }
 
+float inches;
+
 void loop() {
     server.handleClient();
+    // - - - - - - - - - - - - - - - - (Begin)
+    // Distance Sensor Code
+    digitalWrite(TRIG, LOW);    // Turn off the trigger and let things quiet down
+    delay(20);                  // Let it sit for 20 milliseconds
+    digitalWrite(TRIG, HIGH);   // Turn on the trigger to start measurement
+    delayMicroseconds(15);      // Send a very short pulse (15us)
+    digitalWrite(TRIG, LOW);    // Turn off TRIG pin - this will start the ECHO pin high
+    duration = pulseIn(ECHO, HIGH);   // Reads ECHO, returns the travel time in microseconds
+
+    Serial.print("Distance: "); // debug out
+    Serial.print(duration*0.013504/2); // Calculate the distance in inches (13,504 in/s)
+    inches = duration*0.013504/2;
+    Serial.print(" in."); 
+    Serial.println(); // I added this.
+
+    server.send(200, "text/plain", "green");
+
+    //Serial.print("Calculated inches:  "); // I added this.
+    //Serial.println (inches); // I added this.
+    //if (inches > 5) { // I added this.
+    //    Serial.println("Green is for Go!"); // I added this.
+    //} // I added this.
+    //Serial.print("in. [");  // Original; keep this.
+    //Serial.print(duration*0.0343/2);   // Calculate the distance in cm (34,300 cm/s)  // Original; keep this.
+    //Serial.print("cm]\n");  // Original; keep this.
+
+    delay(1000);                // Loop every 1 second
+    // - - - - - - - - - - - - - - - - (End)
 }
 
 void handleMessage() {
@@ -31,15 +78,25 @@ void handleMessage() {
         
         if (data == "redlight") {
             server.send(200, "text/plain", "greenlight");
-        //} else {
-            //server.send(200, "text/plain", "Message received!");
         }
-        //if (data == "test") {
-          char message_to_send[] = "23";  //was "150"
-          server.send(200, "text/plain", message_to_send);
-          //server.send(200, "text/plain", "150");
-          Serial.println("Sent a one-fifty.");
-        //}
+        // This is the area for sending the state.  
+        // Green
+        if (inches > 10) { 
+            Serial.println("Green is for Go!"); 
+            server.send(200, "text/plain", "green");
+        } 
+        // Yellow 
+        if (inches < 10 && inches > 5) { 
+            Serial.println("Yellow is for Yell!"); 
+            server.send(200, "text/plain", "yellow");
+        } 
+        // Red 
+        if (inches < 5) { 
+            Serial.println("Red is for Run!"); 
+            server.send(200, "text/plain", "red");
+        } 
+
+
     } else {
         server.send(400, "text/plain", "No message received");
     }
@@ -48,6 +105,13 @@ void handleMessage() {
 // Note:  what messages does the "stoplight" Arduino need to send to the "distance-sensor"
 // one?  The only thing I can think of would be verifying the connection, and that's 
 // important for safety reasons (so I'll need to build that in as well.)
+
+// Pinout for the "HC-SR04":
+// From front:
+// Vcc | Trig | Echo | Gnd
+// From back:
+// Gnd | Echo | Trig | Vcc
+
 
 
 
--- stoplight/stoplight.ino
+++ stoplight/stoplight.ino
@@ -1,22 +1,46 @@
 #include <ESP8266WiFi.h>
+// - - - - - - - - - - - - - - - - (Begin)
+// LED Control Code
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+// - - - - - - - - - - - - - - - - (End)
 #include <ESP8266HTTPClient.h>
-#include "wi-fi_config.h" // Contains SSID and PASSWORD
+#include "wi-fi_config.h" // Contains WIFI_SSID and WIFI_PASS
 
 WiFiClient client;
 String targetIP = "";
 
+// - - - - - - - - - - - - - - - - (Begin)
+// LED Control Code
+// - - - - - - - - - - - - - - - - (End)
+
+// - - - - - - - - - - - - - - - - (Begin)
+// LED Control Code
+// Define pins for LEDs
+    const int redLED = 16;
+    const int yellowLED = 14;
+    const int greenLED = 12;
+// - - - - - - - - - - - - - - - - (End)
+
+
 void setup() {
     Serial.begin(115200);
     WiFi.begin(WIFI_SSID, WIFI_PASS);
-    
     Serial.print("Connecting to Wi-Fi");
     while (WiFi.status() != WL_CONNECTED) {
         delay(500);
         Serial.print(".");
     }
-    
+    // - - - - - - - - - - - - - - - - (Begin)
+    // LED Control Code
+          // Set each LED pin as an OUTPUT
+          pinMode(redLED, OUTPUT);
+          pinMode(yellowLED, OUTPUT);
+          pinMode(greenLED, OUTPUT);
+        //}
+    // - - - - - - - - - - - - - - - - (End)
     Serial.println("\nConnected! IP Address: " + WiFi.localIP().toString());
-    
     findOtherDevice();
 }
 
@@ -26,11 +50,16 @@ void loop() {
         receiveMessage(targetIP, "Open_for_message");
 
 
-        
-    }
-    delay(1000);
-}
 
+
+
+
+
+
+    }
+    //delay(1000);
+    delay(1);
+}
 
 void findOtherDevice() {
     HTTPClient http;
@@ -51,19 +80,14 @@ void findOtherDevice() {
 bool sendMessage(String ip, String message) {
     HTTPClient http;
     String url = "http://" + ip + "/message?data=" + message;
-    
     http.begin(client, url);
     int httpCode = http.GET();
-    
     if (httpCode > 0) {
         String response = http.getString();
         Serial.println("Response from " + ip + ": " + response);
         if (response == "greenlight") {
             return true;
         }
-        //if (response == "100") {
-        //  Serial.println("It is a one hundred!");
-        //}
     }
     http.end();
     return false;
@@ -72,22 +96,35 @@ bool sendMessage(String ip, String message) {
 bool receiveMessage(String ip, String message) {
     HTTPClient http;
     String url = "http://" + ip + "/message?data=" + message;
-    
     http.begin(client, url);
     int httpCode = http.GET();
-    
     if (httpCode > 0) {
         String response = http.getString();
         Serial.println("Response from " + ip + ": " + response);
         if (response == "greenlight") {
             return true;
         }
-        if (response == "100") {
-          Serial.println("It is a one hundred!");
+        if (response == "green") {
+            digitalWrite(redLED, LOW);
+            digitalWrite(yellowLED, LOW);
+            digitalWrite(greenLED, HIGH);
+          Serial.println("Go green!");
         }
-        if (response == "150") {
-          Serial.println("It is 150!");
+        if (response == "yellow") {
+            digitalWrite(redLED, LOW);
+            digitalWrite(yellowLED, HIGH);
+            digitalWrite(greenLED, LOW);
+          Serial.println("Yell yellow!");
         }
+        if (response == "red") {
+            digitalWrite(redLED, HIGH);
+            digitalWrite(yellowLED, LOW);
+            digitalWrite(greenLED, LOW);
+          Serial.println("Run red!");
+        }
+
+
+
     }
     http.end();
     return false;
